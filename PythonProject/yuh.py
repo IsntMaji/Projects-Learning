5a5f1d5f1267f8ea8ecafa180ad6cc05bc9217d7
IsntMaji <mmorg67176@gmail.com>
2025-07-25 04:03:09 +1000

    Add files via upload

---
+++ PythonProject/yuh.py
@@ -0,0 +1,4 @@
+print("Yo wazzap Beijing, let's generate a platoon name!")
+name1 = input("What is the name of your city?\n")
+name2 = input("What is one of your pet's names?\n")
+print("Your platoon name could be " + name1 + " " + name2 + "'s" + "!")
